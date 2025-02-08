2d4746cc46f6e64afb7e83b6eb8f2716e0775dd6
Vaida-Paul <105378105+Vaida-Paul@users.noreply.github.com>
2025-02-08 23:24:12 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const knex = require("knex");
 const config = require("../knexfile");
 
-const db = knex(config.development);
+// Determine if we are in a production environment or development
+const environment = process.env.NODE_ENV || "development"; // Default to development if not set
+const db = knex(config[environment]); // Use the appropriate configuration based on the environment
 
 module.exports = db;
